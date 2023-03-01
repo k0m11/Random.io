4e021ba1ac87bf541efd72f540d896aaa87203ff
k0m1 <115169534+k0m11@users.noreply.github.com>
2023-03-01 22:09:03 +0300

    Add files via upload

---
+++ static/js/passwords.js
@@ -0,0 +1,13 @@
+let alphabet = 'abcdefghijklmnopqrstuvwxyz'
+let lenghtChoose = document.getElementById("range")
+let sum = document.querySelector("#result")
+
+function render(){
+    let length = lenghtChoose.value
+    let result = ' ';
+    for(let i = 0; i < length; i++) {
+        result += 
+        String.fromCharCode(80 + Math.floor(Math.random() * alphabet.length));
+    }
+    sum.innerHTML = result
+}
